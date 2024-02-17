d6aa912fc29066c60edc0266b5a746acf7ffd230
Newton Ombese <newtonombese1@gmail.com>
2024-02-17 09:51:27 +0300

    Setting up the scroll reveal project

---
+++ ScrollReveal/app.js
@@ -0,0 +1,18 @@
+ScrollReveal({
+  distance: '30px',
+  easing: 'ease-in',
+  origin: 'top',
+  duration: 500,
+})
+
+ScrollReveal().reveal(".title", {
+  delay: 300,
+});
+
+ScrollReveal().reveal(".description", {
+  delay: 800,
+});
+
+ScrollReveal().reveal(".image", {
+  delay: 1200,
+})
